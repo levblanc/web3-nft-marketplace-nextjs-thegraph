836a0ffb9f1814a1a1758414cf6e1fa61e5f2a1a
levblanc <levblanc@gmail.com>
2022-10-16 12:31:06 +0800

    Update home page to show query result from the graph

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import '../styles/globals.css';
 import Head from 'next/head';
 import { MoralisProvider } from 'react-moralis';
-import '../styles/globals.css';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Header from '../components/Header';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: process.env.NEXT_PUBLIC_THE_GRAPH_QUERY_URI,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,8 +18,10 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MoralisProvider initializeOnMount={false}>
-        <Header />
-        <Component {...pageProps} />
+        <ApolloProvider client={client}>
+          <Header />
+          <Component {...pageProps} />
+        </ApolloProvider>
       </MoralisProvider>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,39 @@
 import styles from '../styles/Home.module.css';
+import { useQuery } from '@apollo/client';
+import { useMoralis } from 'react-moralis';
+import { GET_ACTIVE_ITEMS } from '../constants/subgraphQueries';
+import contractAddresses from '../constants/contractAddresses.json';
 
 export default function Home() {
-  return <div className={styles.container}>Hi there!</div>;
+  const { isWeb3Enabled, chainId } = useMoralis();
+  const chain = chainId ? parseInt(chainId) : 31337;
+  const marketplaceAddress = contractAddresses[chain].NFTMarketplace;
+
+  const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS);
+  console.log('data', data);
+  return (
+    <div className="container mx-auto">
+      <h1 className="px-4 py-4 font-bold text-2xl">Recently Listed</h1>
+      <div className="flex flex-wrap">
+        {isWeb3Enabled ? (
+          loading || !data ? (
+            <div>Loading......</div>
+          ) : (
+            data.activeItems.map(({ price, nftAddress, tokenId, seller }) => {
+              return (
+                <div key={`${nftAddress}${tokenId}`}>
+                  <div>{price}</div>
+                  <div>{nftAddress}</div>
+                  <div>{tokenId}</div>
+                  <div>{seller}</div>
+                </div>
+              );
+            })
+          )
+        ) : (
+          <div>Web3 Currently Not Enabled </div>
+        )}
+      </div>
+    </div>
+  );
 }
