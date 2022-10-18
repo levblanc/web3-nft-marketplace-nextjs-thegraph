54d19d31460ae0b47de6a36a0e5d8c95bde07581
levblanc <levblanc@gmail.com>
2022-10-18 11:35:09 +0800

    Add NFT Card UI

---
+++ components/NFTCard.js
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react';
+import { useWeb3Contract, useMoralis } from 'react-moralis';
+import { Card } from '@web3uikit/core';
+import Image from 'next/image';
+import { ethers } from 'ethers';
+import marketplaceAbi from '../constants/marketplaceAbi.json';
+import dynamicNFTAbi from '../constants/dynamicNFTAbi.json';
+
+const NFTBox = ({
+  price,
+  nftAddress,
+  tokenId,
+  // marketplaceAddress,
+  seller,
+}) => {
+  const { isWeb3Enabled } = useMoralis();
+  const [imageTitle, setImageTitle] = useState('');
+  const [imageURI, setImageURI] = useState('');
+  const [imageDesc, setImageDesc] = useState('');
+
+  const { runContractFunction: getTokenURI } = useWeb3Contract({
+    abi: dynamicNFTAbi,
+    contractAddress: nftAddress,
+    functionName: 'tokenURI',
+    params: {
+      tokenId,
+    },
+  });
+
+  /**
+   * @param tokenURI token URI
+   * @returns IPFS files from a 'normal' URL
+   */
+  const ipfsGateway = (tokenURI) => {
+    return tokenURI.replace('ipfs://', 'https://ipfs.io/ipfs/');
+  };
+
+  const updateUI = async () => {
+    const tokenURI = await getTokenURI();
+
+    const {
+      name: imageTitle,
+      image,
+      description: imageDesc,
+    } = await (await fetch(ipfsGateway(tokenURI))).json();
+    const imageURI = ipfsGateway(image);
+
+    setImageTitle(imageTitle);
+    setImageURI(imageURI);
+    setImageDesc(imageDesc);
+  };
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      updateUI();
+    }
+  }, [isWeb3Enabled]);
+
+  return (
+    <div>
+      {imageURI ? (
+        <div className="flex flex-col items-begin mr-5 rounded-lg drop-shadow-lg bg-white">
+          <div className="p-4 bg-yellow-50">
+            <Image
+              loader={() => imageURI}
+              src={imageURI}
+              width={200}
+              height={200}
+              alt={imageDesc}
+            />
+          </div>
+          <div className="font-bold text-slate-600 rounded-b-lg p-2">
+            <div className="text-lg mt-2">PixCats #{tokenId}</div>
+            {/* <div className="italic text-sm">Owned by {seller}</div> */}
+            <div className="text-sm mt-4">Price</div>
+            <div>{ethers.utils.formatUnits(price, 'ether')} ETH</div>
+          </div>
+        </div>
+      ) : (
+        <div>Loading ...</div>
+      )}
+    </div>
+  );
+};
+
+export default NFTBox;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useQuery } from '@apollo/client';
 import { useMoralis } from 'react-moralis';
 import { GET_ACTIVE_ITEMS } from '../constants/subgraphQueries';
 import contractAddresses from '../constants/contractAddresses.json';
+import NFTCard from '../components/NFTCard';
 
 export default function Home() {
   const { isWeb3Enabled, chainId } = useMoralis();
@@ -10,10 +11,10 @@ export default function Home() {
   const marketplaceAddress = contractAddresses[chain].NFTMarketplace;
 
   const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS);
-  console.log('data', data);
+
   return (
-    <div className="container mx-auto">
-      <h1 className="px-4 py-4 font-bold text-2xl">Recently Listed</h1>
+    <div className="container mx-auto text-slate-800">
+      <h1 className="mr-5 my-10 font-bold text-2xl">Recently Listed</h1>
       <div className="flex flex-wrap">
         {isWeb3Enabled ? (
           loading || !data ? (
@@ -21,12 +22,13 @@ export default function Home() {
           ) : (
             data.activeItems.map(({ price, nftAddress, tokenId, seller }) => {
               return (
-                <div key={`${nftAddress}${tokenId}`}>
-                  <div>{price}</div>
-                  <div>{nftAddress}</div>
-                  <div>{tokenId}</div>
-                  <div>{seller}</div>
-                </div>
+                <NFTCard
+                  key={`${nftAddress}${tokenId}`}
+                  price={price}
+                  nftAddress={nftAddress}
+                  tokenId={tokenId}
+                  seller={seller}
+                />
               );
             })
           )
