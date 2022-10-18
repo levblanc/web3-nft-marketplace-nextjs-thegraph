521a0b69bd6de8c5f29603adcc73cea6c7a98a68
levblanc <levblanc@gmail.com>
2022-10-18 18:10:57 +0800

    Update NFTCard to use Ant Design UI

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useWeb3Contract, useMoralis } from 'react-moralis';
-import { Card } from '@web3uikit/core';
+import { CryptoIcon } from 'next-crypto-icons';
+import { Card, Skeleton } from 'antd';
 import Image from 'next/image';
 import { ethers } from 'ethers';
 import marketplaceAbi from '../constants/marketplaceAbi.json';
 import dynamicNFTAbi from '../constants/dynamicNFTAbi.json';
 import { truncateAddress } from '../utils/formatter';
+import UpdateListingModal from './UpdateListingModal';
 
 const NFTBox = ({
   price,
@@ -15,7 +17,7 @@ const NFTBox = ({
   seller,
 }) => {
   const { isWeb3Enabled, account } = useMoralis();
-  const [imageTitle, setImageTitle] = useState('');
+  const [imageLoading, setImageLoading] = useState(true);
   const [imageURI, setImageURI] = useState('');
   const [imageDesc, setImageDesc] = useState('');
 
@@ -37,16 +39,17 @@ const NFTBox = ({
   };
 
   const updateUI = async () => {
+    setImageLoading(true);
+
     const tokenURI = await getTokenURI();
 
-    const {
-      name: imageTitle,
-      image,
-      description: imageDesc,
-    } = await (await fetch(ipfsGateway(tokenURI))).json();
+    const { image, description: imageDesc } = await (
+      await fetch(ipfsGateway(tokenURI))
+    ).json();
+
     const imageURI = ipfsGateway(image);
 
-    setImageTitle(imageTitle);
+    setImageLoading(false);
     setImageURI(imageURI);
     setImageDesc(imageDesc);
   };
@@ -61,31 +64,40 @@ const NFTBox = ({
   const formattedSellerAddress = ownedByUser ? 'you' : truncateAddress(seller);
 
   return (
-    <div>
-      {imageURI ? (
-        <div className="flex flex-col items-begin mr-5 rounded-lg drop-shadow-lg bg-white">
+    <Card className="w-64 mr-5" hoverable>
+      {/* {!imageURI ? ( */}
+      {imageLoading ? (
+        <>
+          <Skeleton.Image active={true} />
+          <Skeleton className="mt-8" active={true} />
+        </>
+      ) : (
+        <>
           <div className="p-4 bg-yellow-50">
             <Image
               loader={() => imageURI}
+              unoptimized={true}
               src={imageURI}
               width={200}
               height={200}
               alt={imageDesc}
             />
           </div>
-          <div className="font-bold text-slate-600 rounded-b-lg p-2">
-            <div className="text-lg mt-2">PixCats #{tokenId}</div>
+          <div className="font-bold text-slate-600 rounded-b-lg px-2">
+            <div className="text-lg mt-4">PixCats #{tokenId}</div>
             <div className="italic text-sm">
               Owned by {formattedSellerAddress}
             </div>
-            <div className="text-sm mt-4">Price</div>
-            <div>{ethers.utils.formatUnits(price, 'ether')} ETH</div>
+            <div className="flex flex-row items-center mt-4 text-lg">
+              <CryptoIcon name="eth" width={18} style={'color'} />
+              <div className="ml-2">
+                {ethers.utils.formatUnits(price, 'ether')} ETH
+              </div>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div>Loading ...</div>
+        </>
       )}
-    </div>
+    </Card>
   );
 };
 
