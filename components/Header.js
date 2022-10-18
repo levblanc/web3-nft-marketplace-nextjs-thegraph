12edeb244e536fcd6c04c50e4509c3e22b9f6f6b
levblanc <levblanc@gmail.com>
2022-10-18 18:09:57 +0800

    Refine Header styles

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ import { ConnectButton } from '@web3uikit/web3';
 
 const Header = () => {
   return (
-    <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
-      <h1 className="px-4 py-4 font-bold text-3xl">NFT Marketplace</h1>
-      <div className="flex flex-row items-center">
+    <nav className="px-5 py-3 border-b border-slate-200 flex flex-row justify-between items-center">
+      <h1 className="h-3 leading-3 font-bold text-3xl">NFT Marketplace</h1>
+      <div className="flex flex-row items-center text-lg">
         <Link href="/">
           <a className="mr-4 p-5">Home</a>
         </Link>
