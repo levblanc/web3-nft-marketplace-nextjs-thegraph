f0f9af5c007a9b785adb2e05360c57a165a13e39
levblanc <levblanc@gmail.com>
2022-10-27 18:55:12 +0800

    Update footer link class name

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <a className="footerLink" href="" target={'_blank'}>
+      <a className="link" href="" target={'_blank'}>
         Built by Levblanc
       </a>
       <i className="githubIcon"></i>
